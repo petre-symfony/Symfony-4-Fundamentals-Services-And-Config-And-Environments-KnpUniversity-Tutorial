9d7d027c1c53c75fd5b8ab138fda05943639b259
petrero <petrero@gmail.com>
2018-02-11 11:50:19 +0200

    Copy code from branch 10

---
+++ src/Service/MarkdownHelper.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Service;
+
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
+
+class MarkdownHelper {
+  private $cache;
+  private $markdown;
+  private $logger;
+  
+  public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger) {
+    $this->cache = $cache;
+    $this->markdown = $markdown;
+    $this->logger = $markdownLogger;
+  }
+
+  public function parse(string $source):string{
+    if(stripos($source, 'bacon') !== FALSE){
+      $this->logger->info('They are talking about bacon again!');
+    }
+    
+    $item = $this->cache->getItem('markdown_'.md5($source));
+    if (!$item->isHit()){
+      $item->set($this->markdown->transform($source));
+      $this->cache->save($item);
+    }
+
+    return $item->get();
+  }
+}
