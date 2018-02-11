4961585c4fc91359ed5742e87e09d0bd1d603dfd
petrero <petrero@gmail.com>
2018-02-11 10:09:03 +0200

    Dependency Injection: The Wrong Way First
    In MarkdownHelper, oh, update the code to use the  variable. Here's the problem: MarkdownHelper needs the cache and markdown services. To say it differently, they're dependencies. So how can we get them from here?
    
    Symfony follows object-orientated best practices... which means that there's no way to magically fetch them out of thin air. But that's no problem! If you ever need a service or some config, just pass them in.
    
    The easiest way to do this is to add them as arguments to parse(). I'll show you a different solution in a minute - but let's get it working. Add AdapterInterface  and MarkdownInterface .
    
    If you try it now... it fails:
    
        Too few arguments passed to parse(): 1 passed, 3 expected.

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -1,12 +1,14 @@
 <?php
 namespace App\Service;
 
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Michelf\MarkdownInterface;
 
 class MarkdownHelper {
-  public function parse(string $source):string{
-    $item = $cache->getItem('markdown_'.md5($articleContent));
+  public function parse(string $source, AdapterInterface $cache, MarkdownInterface $markdown):string{
+    $item = $cache->getItem('markdown_'.md5($source));
     if (!$item->isHit()){
-      $item->set($markdown->transform($articleContent));
+      $item->set($markdown->transform($source));
       $cache->save($item);
     }
 
